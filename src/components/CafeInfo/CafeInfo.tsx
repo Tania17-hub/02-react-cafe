617e18a0961a48de33fa36ecd850ad0a710189a8
Тетяна <yabanshi.tania@gmail.com>
2025-07-06 15:00:14 +0300

    updare all files

---
+++ src/components/CafeInfo/CafeInfo.tsx
@@ -0,0 +1,8 @@
+const CafeInfo = () => (
+  <section>
+    <h1>Добро пожаловать в React Cafe</h1>
+    <p>Оцініть наш сервіс, будь ласка!</p>
+  </section>
+);
+
+export default CafeInfo;
---
+++ src/components/Notification/Notification.tsx
@@ -0,0 +1,7 @@
+const Notification = () => (
+  <section>
+    <p>Немає відгуків поки що.</p>
+  </section>
+);
+
+export default Notification;
